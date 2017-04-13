535bdc5fe468bb92fbdb12ad15534471cdf7b4d1
caqeenee <cahqeenee@gmail.com>
2017-04-13 16:54:51 +0700

    Update safelink-convrt-multi.js

--- safelink-convrt-multi.js
+++ safelink-convrt-multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="1.html"}
-if(x=="2"){xxx="2.html"}
-if(x=="3"){xxx="3.html"}
-if(x=="4"){xxx="4.html"}
-if(x=="5"){xxx="5.html"}a_to_ve[i].href="http://safelink-convrt.blogspot.com/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="1a.html"}
+if(x=="2"){xxx="2a.html"}
+if(x=="3"){xxx="3a.html"}
+if(x=="4"){xxx="4a.html"}
+if(x=="5"){xxx="5a.html"}a_to_ve[i].href="http://safelink-convrt.blogspot.com/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
